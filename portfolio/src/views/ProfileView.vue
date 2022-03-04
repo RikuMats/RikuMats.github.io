30fdfc604671a1819b323d6073b37120baf4de3f
RikuMats <al18108@shibaura-it.ac.jp>
2022-03-04 11:37:04 +0900

    remove some files.minimize HelloWorldCompornt.

--- portfolio/src/views/ProfileView.vue
+++ portfolio/src/views/ProfileView.vue
@@ -3,13 +3,15 @@
     <h1>Profile</h1>
     <img alt="temporary img" src="../assets/logo.png" />
     <p>name: Riku</p>
+    <HelloWorld></HelloWorld>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "@/components/HelloWorld.vue";
 @Component({
-  components: {},
+  components: { HelloWorld },
 })
 export default class ProfileView extends Vue {}
 </script>
