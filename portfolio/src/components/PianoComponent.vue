c64da63d250bcf4b555730338bec57f44b8aa403
RikuMats <al18108@shibaura-it.ac.jp>
2022-03-04 14:54:12 +0900

    start to create piano component

---
+++ portfolio/src/components/PianoComponent.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="piano-container">
+    <PianoKey
+      v-for="(keyColor, keyId) in keyMaps"
+      :keyId="keyId"
+      :keyColor="keyColor"
+    ></PianoKey>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorater";
+import PianoKey from "@/components/PianoKey.vue";
+@Component({
+  components: { PianoKey },
+})
+export default class PianoComponent extends Vue {
+  private keyMaps = [
+    {
+      keyColor: "white",
+      keyId: "1-do",
+    },
+    {
+      keyColor: "black",
+      keyId: "1-do-s",
+    },
+    {
+      keyColor: "white",
+      keyId: "1-re",
+    },
+    {
+      keyColor: "black",
+      keyId: "1-re-s",
+    },
+    {
+      keyColor: "white",
+      keyId: "1-mi",
+    },
+    {
+      keyColor: "white",
+      keyId: "1-fa",
+    },
+    {
+      keyColor: "black",
+      keyId: "1-fa-s",
+    },
+    {
+      keyColor: "white",
+      keyId: "1-so",
+    },
+    {
+      keyColor: "black",
+      keyId: "1-so-s",
+    },
+    {
+      keyColor: "white",
+      keyId: "1-la",
+    },
+    {
+      keyColor: "black",
+      keyId: "1-la-s",
+    },
+    {
+      keyColor: "white",
+      keyId: "1-ti",
+    },
+  ];
+  get size(): number {
+    return this.keyMaps.length;
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ portfolio/src/components/PianoKey.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="piano-key">
+    <span class="key-label">{{ keyName }}</span>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Prop } from "vue-property-decorator";
+
+export default class PianoKey extends Vue {
+  @Prop({ required: true }) public keyColor!: string; //white or black
+  @Prop({ required: true }) public keyId!: string; //1-ドetc
+  private isPressed = false;
+  public press(): void {
+    alert(this.keyId);
+  }
+  public release(): void {
+    console.log("released");
+  }
+  get keyClass(): string {
+    return this.isPressed ? "default" : "pressed";
+  }
+  get keyName(): string {
+    return this.keyId.replace(/\d+-/, "").replace(/-s/, "#");
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
