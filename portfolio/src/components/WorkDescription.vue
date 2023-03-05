ba14ed46e60447ab99d37b6606c15a9d8acaa90e
RikuMats <al18108@shibaura-it.ac.jp>
2023-03-05 22:46:28 +0900

    add work component

---
+++ portfolio/src/components/WorkDescription.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container>
+    <div class="text-h4">
+      <slot name="title"></slot>
+    </div>
+    <div class="text-body2">
+      <slot name="description"></slot>
+    </div>
+    <slot name="content"></slot>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component
+export default class WorkDescription extends Vue {}
+</script>
--- portfolio/src/views/WorksView.vue
+++ portfolio/src/views/WorksView.vue
@@ -2,15 +2,33 @@
   <v-container>
     <h1>Works</h1>
     <v-divider></v-divider>
-    <PianoComponent></PianoComponent>
+    <WorkDescription>
+      <template v-slot:title> Vue Piano </template>
+      <template v-slot:description> Vue で作ったピアノ</template>
+      <template v-slot:content>
+        <PianoComponent></PianoComponent>
+      </template>
+    </WorkDescription>
+    <WorkDescription>
+      <template v-slot:title>Books Diary</template>
+      <template v-slot:description>
+        バックエンドにPHP、フロントエンドにvueを使ったアプリケーション
+        <br />
+        デモページはフロントのみなのでidとパスワードは任意の文字列
+      </template>
+      <template v-slot:content>
+        <a href="http://rikumats.github.io/book_diary_view/#/">デモページ </a>
+      </template>
+    </WorkDescription>
   </v-container>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import PianoComponent from "@/components/PianoComponent.vue";
+import WorkDescription from "@/components/WorkDescription.vue";
 @Component({
-  components: { PianoComponent },
+  components: { PianoComponent, WorkDescription },
 })
 export default class SkillsView extends Vue {}
 </script>
